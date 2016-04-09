894bfbd6d0c7f4cae6ba77e608bc8cda4979c5d1
Hamilton <hamdiggy@gmail.com>
2016-04-09 15:35:23 -0700

    Update example.php

--- example.php
+++ example.php
@@ -1,9 +1,12 @@
 <?php
 
-include("ham-package.php");
+include_once("ham-package.php");
 
 $hp = new HamPackage();
 
+// Add text content to the stream
+$hp->append("Version 1.0");
+// Gather text content of files into the stream
 $hp->gather(
 	array(
 		"project/main.php",
@@ -12,11 +15,11 @@ $hp->gather(
 	)
 );
 
+// Build multiple packages to multiple file names
+$hp->buildPackage("project.full.php");
+$hp->buildPackage("project.min.php", $hp->minify(););
+
+// Run a local copy so that the build script can also be used
 $hp->runLocal();
-
-$hp->runPackage("project.full.php");
-
-$hp->minify();
-$hp->runPackage("project.min.php");
 
 ?>
